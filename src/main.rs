fbe81c2fcdec09d8e89c3a4e656b972da7dabb34
cmsong <cmsong@shinasys.com>
2023-02-06 17:57:41 +0900

    chore: 프로그램 시작시 디버깅  로그 제거

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,6 @@ use clap::Parser;
 fn main() {
     let cli = Cli::parse();
 
-    println!("{:?}", cli.command);
-
     match cli.command {
         None => mvdup::cli::mvdup(cli),
         Some(Commands::Update { path, verify }) => update(path, verify),
